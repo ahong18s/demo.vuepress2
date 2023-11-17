01205ccff959348c9adb05ecd2b550d3eef05997
ahong18s <ahong18s@163.com>
2023-11-17 19:17:34 +0800

    去除旧版本的插件

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,9 +1,8 @@
 import {defineUserConfig, defaultTheme} from 'vuepress';
 import {registerComponentsPlugin} from '@vuepress/plugin-register-components';
 import {resolve} from 'path';
-import {copyCodePlugin} from "vuepress-plugin-copy-code2";
 import {docsearchPlugin} from '@vuepress/plugin-docsearch';
-import { shikiPlugin } from '@vuepress/plugin-shiki';
+import {shikiPlugin} from '@vuepress/plugin-shiki';
 // 引入配置
 import {navbarZh, navbarEn,} from './config/navbar/index';
 import {sidebarZh, sidebarEn,} from './config/sidebar/index';
@@ -13,9 +12,8 @@ const base = '/';
 
 export default defineUserConfig({
     base: base,
-    title: '',
     description: '',
-    lang: 'zh-CN',
+    dest: 'docs',
     head: [
         ['link', {rel: 'icon', href: '/logo.svg', type: 'image/png'}],//sizes: '16x16'
         //完全地兼容PWA
@@ -30,10 +28,8 @@ export default defineUserConfig({
         ['meta', {name: 'msapplication-TileImage', content: '/logo.svg'}],
         ['meta', {name: 'msapplication-TileColor', content: '#3eaf7c'}],
     ],
-    host: '0.0.0.0',//本地服务
-    port: 8888,//本地服务
-    dest: 'docs',
-    // 语言配置
+    host: '0.0.0.0',
+    lang: 'zh-CN',//本地服务
     locales: {
         '/': {
             lang: 'zh-CN',
@@ -45,96 +41,7 @@ export default defineUserConfig({
             title: 'VuePress',
             description: 'Vue-powered Static Site Generator',
         },
-    },
-    // 主题配置
-    theme: defaultTheme({
-        docsDir: 'docs',
-        logo: '/logo.png',
-        // logoDark: '',
-        repo: 'https://github.com/ahong18s/demo.vuepress2',
-        repoLabel: 'GitHub',
-        locales: {
-            '/': {
-                colorMode: 'auto',
-                colorModeSwitch: true,
-                docsDir: 'src',
-                editLink: true,
-                editLinkText: '在 GitHub 上编辑此页',//编辑链接文字
-                // editLinkPattern:'',
-                navbar: navbarZh,
-                // repoLabel: '',
-                // 多语言下拉菜单的标题
-                selectLanguageAriaLabel: '选择语言',
-                selectLanguageName: '简体中文',
-                selectLanguageText: '选择语言',
-                sidebar: sidebarZh,
-                sidebarDepth: 2,//默认1，侧边栏会自动地显示由当前页面的标题（headers）组成的链接
-                // page meta
-                lastUpdated: true,
-                lastUpdatedText: '上次更新',
-                contributorsText: '贡献者',
-                // custom containers
-                tip: '提示',
-                warning: '注意',
-                danger: '警告',
-                // 404 page
-                notFound: [
-                    '这里什么都没有',
-                    '我们怎么到这来了？',
-                    '这是一个 404 页面',
-                    '看起来我们进入了错误的链接',
-                ],
-                backToHome: '返回首页',
-                // a11y
-                openInNewWindow: '在新窗口打开',
-                toggleColorMode: '切换颜色模式',
-                toggleSidebar: '切换侧边栏',
-            },
-            '/en-US/': {
-                colorMode: 'auto',
-                colorModeSwitch: true,
-                docsDir: 'src',
-                editLink: false,
-                editLinkText: 'Edit this page on GitHub',
-                // editLinkPattern:'',
-                navbar: navbarEn,
-                // repoLabel: '',
-                // Configuration of Service Worker
-                selectLanguageAriaLabel: 'Languages',
-                selectLanguageText: 'Languages',
-                selectLanguageName: 'English',//The label of the language in the drop-down menu
-                sidebar: sidebarEn,
-                sidebarDepth: 2,//默认1，侧边栏会自动地显示由当前页面的标题（headers）组成的链接
-                // page meta
-                lastUpdated: true,
-                lastUpdatedText: 'last update',
-                contributorsText: 'contributor',
-                // custom containers
-                tip: 'prompt',
-                warning: 'warning',
-                danger: 'danger',
-                // 404 page
-                notFound: [
-                    'There\'s nothing here',
-                    'How did we get here?',
-                    'This is a 404 page',
-                    'Looks like we entered the wrong link',
-                ],
-                backToHome: 'homepage',
-                // a11y
-                openInNewWindow: 'Open in a new window',
-                toggleColorMode: 'Switch color mode',
-                toggleSidebar: 'Switch Sidebar',
-            },
-        },
-        themePlugins: {
-            // only enable git plugin in production mode
-            git: isProd,
-            // use shiki plugin in production mode instead
-            prismjs: !isProd,
-        },
-    }),
-    // 语法配置
+    },//本地服务
     markdown: {
         importCode: {
             handleImportPath: (str) =>
@@ -144,24 +51,14 @@ export default defineUserConfig({
             highlightLines: true
         }
     },
+    // 语言配置
     plugins: [
         registerComponentsPlugin({
             components: {
                 NpmBadge: resolve(__dirname, './components/NpmBadge.vue'),
             },
         }),
-        copyCodePlugin({
-            locales: {
-                "/": {
-                    copy: "复制此段代码",
-                },
-
-                "/en-US/": {
-                    copy: "Copy this code segment",
-                },
-            },
-        }),
-        isProd ? shikiPlugin({ theme: 'dark-plus' }) : [],
+        isProd ? shikiPlugin({theme: 'dark-plus'}) : [],
         // ["md-enhance", {codegroup: true}],
         docsearchPlugin({
             appId: 'H2L64TMNCG',
@@ -265,4 +162,95 @@ export default defineUserConfig({
             },
         }),
     ],
+    // 主题配置
+    port: 8888,
+    // 语法配置
+    theme: defaultTheme({
+        docsDir: 'docs',
+        logo: '/logo.png',
+        // logoDark: '',
+        repo: 'https://github.com/ahong18s/demo.vuepress2',
+        repoLabel: 'GitHub',
+        locales: {
+            '/': {
+                colorMode: 'auto',
+                colorModeSwitch: true,
+                docsDir: 'src',
+                editLink: true,
+                editLinkText: '在 GitHub 上编辑此页',//编辑链接文字
+                // editLinkPattern:'',
+                navbar: navbarZh,
+                // repoLabel: '',
+                // 多语言下拉菜单的标题
+                selectLanguageAriaLabel: '选择语言',
+                selectLanguageName: '简体中文',
+                selectLanguageText: '选择语言',
+                sidebar: sidebarZh,
+                sidebarDepth: 2,//默认1，侧边栏会自动地显示由当前页面的标题（headers）组成的链接
+                // page meta
+                lastUpdated: true,
+                lastUpdatedText: '上次更新',
+                contributorsText: '贡献者',
+                // custom containers
+                tip: '提示',
+                warning: '注意',
+                danger: '警告',
+                // 404 page
+                notFound: [
+                    '这里什么都没有',
+                    '我们怎么到这来了？',
+                    '这是一个 404 页面',
+                    '看起来我们进入了错误的链接',
+                ],
+                backToHome: '返回首页',
+                // a11y
+                openInNewWindow: '在新窗口打开',
+                toggleColorMode: '切换颜色模式',
+                toggleSidebar: '切换侧边栏',
+            },
+            '/en-US/': {
+                colorMode: 'auto',
+                colorModeSwitch: true,
+                docsDir: 'src',
+                editLink: false,
+                editLinkText: 'Edit this page on GitHub',
+                // editLinkPattern:'',
+                navbar: navbarEn,
+                // repoLabel: '',
+                // Configuration of Service Worker
+                selectLanguageAriaLabel: 'Languages',
+                selectLanguageText: 'Languages',
+                selectLanguageName: 'English',//The label of the language in the drop-down menu
+                sidebar: sidebarEn,
+                sidebarDepth: 2,//默认1，侧边栏会自动地显示由当前页面的标题（headers）组成的链接
+                // page meta
+                lastUpdated: true,
+                lastUpdatedText: 'last update',
+                contributorsText: 'contributor',
+                // custom containers
+                tip: 'prompt',
+                warning: 'warning',
+                danger: 'danger',
+                // 404 page
+                notFound: [
+                    'There\'s nothing here',
+                    'How did we get here?',
+                    'This is a 404 page',
+                    'Looks like we entered the wrong link',
+                ],
+                backToHome: 'homepage',
+                // a11y
+                openInNewWindow: 'Open in a new window',
+                toggleColorMode: 'Switch color mode',
+                toggleSidebar: 'Switch Sidebar',
+            },
+        },
+        themePlugins: {
+            // only enable git plugin in production mode
+            git: isProd,
+            // use shiki plugin in production mode instead
+            prismjs: !isProd,
+        },
+    }),
+    title: '',
 });
